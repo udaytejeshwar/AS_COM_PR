5fe6bf17be075898a0fee12fe15d860e4b44e109
udaytejeshwar <67693720+udaytejeshwar@users.noreply.github.com>
2025-07-07 22:23:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Needed for custom domain (e.g., arkspindles.com)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
