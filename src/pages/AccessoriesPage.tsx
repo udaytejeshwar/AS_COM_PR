9269c9255b092b7a9d7b19068f66bcdd28c91f07
udaytejeshwar <67693720+udaytejeshwar@users.noreply.github.com>
2025-10-16 22:59:10 +0530

    Update AccessoriesPage.tsx

--- src/pages/AccessoriesPage.tsx
+++ src/pages/AccessoriesPage.tsx
@@ -35,7 +35,7 @@ const AccessoriesPage = () => {
         <div className="container mx-auto px-4 pt-40 pb-24 sm:px-6 lg:px-8 relative z-10">
           <div className="flex justify-between items-start">
             <div>
-              <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 leading-tight animate-slide-in">
+              <h1 className="text-3xl sm:text-5xl lg:text-5xl font-light mb-6 leading-tight animate-slide-in">
                 Spindle Accessories
               </h1>
               <p className="text-xl mb-8 text-gray-200 max-w-2xl">
